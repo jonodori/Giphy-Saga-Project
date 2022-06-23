d2d5f12b55708d721f20cd91bd2dece4e5297413
Andrew Leck <Aleck360@gmail.com>
2022-06-23 14:23:39 -0500

    Created wireframe for homepage

---
+++ src/components/App/HomePage/HomePage.jsx
@@ -0,0 +1,24 @@
+function HomePage(){
+    return(
+        <>
+            <div>
+                <h1>Giphy Search!</h1>
+                <div>
+                    <button> View Favorites</button>
+                </div>
+            </div> 
+                <div>
+                    <input type='text' placeholder='Enter name'/>
+                    <span><button>search</button></span>
+                </div>
+                <section>
+                    <h2> Here are your Images!</h2>
+                    <div>
+                        Images go here
+                    </div>
+                </section>
+        </>
+    )
+}
+
+export default HomePage;
