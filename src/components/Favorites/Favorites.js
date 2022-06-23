5f1f3cc91dcb90836f98bc243167e65ad1107a32
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-23 13:54:03 -0500

    start of wire frame

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,18 @@
+
+function FavoritesList() {
+
+
+    return (
+
+        <div>
+            <h3>Favorites</h3>
+
+            <ul>
+                <li> </li>
+            </ul>
+
+        </div>
+    );
+}
+
+export default FavoritesList;
