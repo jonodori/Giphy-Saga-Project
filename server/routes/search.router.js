c32e320b1d3260f0152ef7a166349d2036d8fb66
Andrew Leck <Aleck360@gmail.com>
2022-06-24 11:37:54 -0500

    almost got it working

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -4,23 +4,24 @@ const axios = require('axios');
 require('dotenv').config();
 
 
-router.get('/', (req, res) => {
-
+router.get('/:searchBox', (req, res) => {
+    console.log(req.params.searchBox)
+    let tacos = req.params.searchBox
 axios({
     method: "GET",
     url: 'https://api.giphy.com/v1/gifs/search',
     params: {
         api_key: process.env.GIPHY_API_KEY,
-        q: 
+        q: tacos,
         limit: 5
     }
 })
     .then(response => {
-        console.log(response);
+        // console.log(response);
         res.send(response.data);
     })
     .catch(err => {
-        console.error('No gifs for you', err);
+        // console.error('No gifs for you', err);
         res.sendStatus(500);
     });
 
